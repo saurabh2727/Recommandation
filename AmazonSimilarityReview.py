88856203784d10e636e73bfdc8b7e04e7fa730d0
saurabh2727 <47946119+saurabh2727@users.noreply.github.com>
2020-08-17 03:44:35 +1000

    Update AmazonSimilarityReview.py

--- AmazonSimilarityReview.py
+++ AmazonSimilarityReview.py
@@ -1,9 +1,3 @@
-# To run on 20 EMR nodes:
-# !python MovieSimilaritiesLarge.py -r emr --num-ec2-instances=20 --items=ml-1m/movies.dat ml-1m/ratings.dat
-
-# Troubleshooting EMR jobs (subsitute your job ID):
-# !python -m mrjob.tools.emr.fetch_logs --find-failure j-1NXMMBNEQHAFT
-
 
 from mrjob.job import MRJob
 from mrjob.step import MRStep
